16773410d1843f682c0e450939da9adec088fee8
Zeploc <alexcoultasis@gmail.com>
2018-08-29 11:46:30 +1200

    - Added Forge System

---
+++ Source/UnderForge/Items/ForgeItem.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ForgeItem.generated.h"
+
+UCLASS()
+class UNDERFORGE_API AForgeItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AForgeItem();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
---
+++ Source/UnderForge/Items/ForgePart.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ForgePart.h"
+
+
+// Sets default values
+AForgePart::AForgePart()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AForgePart::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AForgePart::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
