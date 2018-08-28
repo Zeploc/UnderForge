32301702bf6406915b21fd3462bc171b4f9bc0ab
Zeploc <alexcoultasis@gmail.com>
2018-08-29 10:38:05 +1200

    - Added Base Player Stuffs

---
+++ Source/UnderForge/Player/ForgePlayer.cpp
@@ -0,0 +1,70 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ForgePlayer.h"
+//#include "Components/InputComponent.h"
+
+
+// Sets default values
+AForgePlayer::AForgePlayer()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AForgePlayer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AForgePlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AForgePlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// set up gameplay key bindings
+	PlayerInputComponent->BindAxis("MoveRight", this, &AForgePlayer::MoveRight);
+	PlayerInputComponent->BindAxis("MoveUp", this, &AForgePlayer::MoveUp);
+	
+	PlayerInputComponent->BindAction("Interact", IE_Pressed, this, &AForgePlayer::Interact);
+}
+void AForgePlayer::MoveRight(float Value)
+{
+	// add movement in that direction
+	AddMovementInput(FVector(1.f, 0.f, 0.f), Value);
+}
+
+void AForgePlayer::MoveUp(float Value)
+{
+	// add movement in that direction
+	AddMovementInput(FVector(0.f, -1.f, 0.f), Value);
+}
+
+void AForgePlayer::Interact()
+{
+	FHitResult hit;
+	FVector EndLocation = GetActorLocation() + GetActorRotation().Vector() * InteractRange;
+	FCollisionQueryParams Traceparams(TEXT("Interact Trace"), false, this);
+
+	GetWorld()->LineTraceSingleByChannel(hit, GetActorLocation(), EndLocation, ECC_GameTraceChannel1, Traceparams);
+	AInteractActor* InteractActor = Cast<AInteractActor>(hit.Actor);
+	if (InteractActor)
+	{
+		InteractActor->Interact();
+		GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Cyan, TEXT("Interact with interact actor"));
+	}
+	else if (hit.bBlockingHit)
+	{
+		//GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Cyan, TEXT("Interact Hit object " + hit.Actor.Get()->GetFName().ToString()));
+	}
+
+}
---
+++ Source/UnderForge/Player/ForgePlayer.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ForgePlayer.generated.h"
+
+UCLASS()
+class UNDERFORGE_API AForgePlayer : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AForgePlayer();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	/** Called for side to side input */
+	void MoveRight(float Val);
+
+	/** Called for side to side input */
+	void MoveUp(float Val);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+	void Interact();
+	
+};
