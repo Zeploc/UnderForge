a2a07c7c2057cad4a475ab6f43f64934ec340a4a
DontLookAthis <DontLookAthis@users.noreply.github.com>
2018-08-29 16:40:40 +1200

    jhonty merge

--- Source/UnderForge/Level/ResourcePile.cpp
+++ Source/UnderForge/Level/ResourcePile.cpp
@@ -1,7 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ResourcePile.h"
+#include "Items/ForgeMat.h"
+#include "Engine/World.h"
 
-
-
-
+AForgeMat * AResourcePile::GetResource()
+{
+	AForgeMat* ResourceRef = GetWorld()->SpawnActor<AForgeMat>(ForgeMat, ObjectPosition->GetComponentLocation(), ObjectPosition->GetComponentRotation());
+	return ResourceRef;
+}
